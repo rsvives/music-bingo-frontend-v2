3c0cc7a54967353cbd852530c8fc649322a9fc70
Rodri <r.serranvives@gmail.com>
2025-09-10 02:30:16 +0200

    refactor: bingo card component

---
+++ src/components/BingoCard.tsx
@@ -0,0 +1,23 @@
+import { BingoNumber } from "./BingoNumber"
+
+type props = {
+    bingoNumbers: Array<Array<number>>
+}
+
+
+export const BingoCard = ({ bingoNumbers }: props) => {
+
+
+    return (
+        <table cellSpacing={0} cellPadding={0} border={0}>
+            <tbody>
+
+                {bingoNumbers.map((row, i) =>
+                    <tr key={`row-${i}`}>
+                        {row.map((number, j) => <BingoNumber key={`col-${i}-${j}`} number={number} row={i} col={j} />)}
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    )
+}
