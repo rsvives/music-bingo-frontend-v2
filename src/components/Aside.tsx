2b769d4935fe25b4840f74f4b90d22d1610fe130
Rodri <r.serranvives@gmail.com>
2025-09-12 00:22:56 +0200

    refactor: clean up Aside component by removing unused code and improving structure

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,48 +1,36 @@
 import { useEffect } from "react"
 import { PlayerList } from "./PlayersList"
+import { JoinLeaveButton } from "./JoinLeaveButton"
 import { useAuthStore } from "@/store/useAuthStore"
 import { useGameStore } from "@/store/useGameStore"
 
 export const Aside = () => {
-    const { socket, authUser } = useAuthStore()
-    const { admin, addPlayer, setJoined } = useGameStore()
 
-
-
-    const joinedToRoom = () => {
-        console.log('joinedtoroom')
-        if (socket) {
-            socket.on('room:joined', (data) => {
-                console.log('room:joined', data)
-                const { lastPlayerJoined } = data
-                addPlayer(lastPlayerJoined)
-                setJoined(true)
-            })
-
-        }
-    }
-
-    useEffect(joinedToRoom, [socket])
-
-    if (admin === null || authUser === null) return <div>Loading...</div>
-
-    // const isAdmin = authUser.id === admin.id ? true : false
+    // if (admin === null || authUser === null) return <div>Loading...</div>
 
     return (
         <>
             <aside className='p-2 flex flex-col gap-4 sm:max-w-[280px] w-full'>
-                {/* <div className='flex gap-2 justify-between items-end p-2 rounded-lg border-1 border-slate-200'>
-                <div>
-                        <h1><b>Room:</b> <code>{roomId}</code></h1>
-                        <p><b>Code:</b> <code>{code}</code> </p>
-                    </div>
-                {gameStatus === 'waiting' && !isAdmin &&
-                        <JoinLeaveButton isJoined={isJoined} handleJoin={handleJoin} />
-                    }
-                </div> */}
+                {/* <RoomDetails /> */}
                 <PlayerList />
             </aside>
         </>
     )
 }
 
+const RoomDetails = () => {
+    const { authUser } = useAuthStore()
+    const { roomId, code, gameStatus, admin } = useGameStore()
+    const isAdmin = authUser?.id === admin?.id
+    return (
+        <div className='flex gap-2 justify-between items-end p-2 rounded-lg border-1 border-slate-200'>
+            <div>
+                <h1><b>Room:</b> <code>{roomId}</code></h1>
+                <p><b>Code:</b> <code>{code}</code> </p>
+            </div>
+            {gameStatus === 'waiting' && !isAdmin &&
+                <JoinLeaveButton />
+            }
+        </div>
+    )
+}
