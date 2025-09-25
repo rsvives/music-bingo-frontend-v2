38366a5eb175c7f8fba1bc94f748b7973e3b8dca
Rodri <r.serranvives@gmail.com>
2025-09-25 17:49:00 +0200

    feat: players card name clipped with ellipsis

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -13,12 +13,12 @@ export const PlayerCard = ({ player, showProgress = true, showIsAdmin = true }: 
     console.log('progress', progress)
     return (
         <div className=" sm:w-full gap-4  border-slate-200 border-1 rounded-lg overflow-hidden">
-            <div className="flex gap-4 justify-between items-center p-1.5">
-                <p className="flex gap-2 items-center text-ellipsis ">
-                    <img src={player.pic} alt={`${player.firstName} profile pic`} className="w-[24px] rounded-full aspect-square" />
-                    {player.firstName}
-                </p>
-                {showIsAdmin && player.isAdmin && <span className="text-[9px] sm:text-xs font-bold p-1 rounded-full border-1 border-slate-100 text-slate-700" title="admin"><AdminCrownIcon size={14} color="var(--color-amber-400)" /></span>}
+            <div className="flex gap-1 justify-between items-center p-1.5">
+                <div className="flex gap-2 items-center  min-w-[24px]">
+                    <img src={player.pic} alt={`${player.firstName} profile pic`} className="w-[24px] min-w-[24px] rounded-full aspect-square basis-6" />
+                    <p className="text-ellipsis whitespace-nowrap overflow-hidden">{player.firstName}</p>
+                </div>
+                {showIsAdmin && player.isAdmin && <span className="p-1 rounded-full border-1 border-slate-100 text-slate-700 basis-2" title="admin"><AdminCrownIcon size={14} color="var(--color-amber-400)" /></span>}
             </div>
             {
                 showProgress && <div className={'h-[2px] bg-slate-400'} style={{ width: progress + '%' }}></div>
