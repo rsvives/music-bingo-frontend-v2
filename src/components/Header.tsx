d05ef7c35f3bd50344f1a4c4b871b9095ff5454e
Rodri <r.serranvives@gmail.com>
2025-09-29 19:44:44 +0200

    feat: added confirmation and socket events to return back to home link (flabingo logo)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,43 @@
-import { Link } from '@tanstack/react-router'
+import { useNavigate } from '@tanstack/react-router'
 import ClerkHeader from '../integrations/clerk/header-user.tsx'
-// import { usePlayersStore } from '@/store/usePlayersStore.ts'
+import socket from '@/socket/socket.ts'
+import { isAdmin } from '@/lib/utils.ts'
+import { useGameStore } from '@/store/useGameStore.ts'
 
 
 export default function Header() {
-  // const { currentPlayer } = usePlayersStore()
 
+  const navigate = useNavigate()
+
+  const handleGoHome = () => {
+    const baseText = 'Are you sure you want to leave?'
+    const confirmText = isAdmin() ?
+      `${baseText} This action will delete the room and its players` : baseText
+
+    // TODO: refactor this shit
+    if (useGameStore.getState().gameStatus !== 'waiting' || useGameStore.getState().gameStatus !== 'ended') {
+      const confirmed = confirm(confirmText)
+      if (isAdmin()) socket.emit('game:paused')
+      if (confirmed) {
+        if (isAdmin()) {
+          socket.emit('game:end')
+        } else {
+          socket.emit('room:leave')
+        }
+        navigate({ to: '/', reloadDocument: true, replace: true })
+      } else {
+        if (isAdmin()) socket.emit('game:resumed')
+      }
+
+    } else {
+      navigate({ to: '/', reloadDocument: true, replace: true })
+    }
+  }
   return (
     <header className="p-2 flex gap-2 bg-white text-black justify-between items-center">
-      <Link to='/' reloadDocument={true}>
+      <button onClick={handleGoHome}>
         <img src='/logo.svg' className='h-[48px]'></img>
-      </Link>
-      {/* {currentPlayer ?? 'john doe'} */}
-      {/* {process.env.NODE_ENV !== 'production' && <span>{socket?.id}</span>} */}
+      </button>
       <div>
         <ClerkHeader />
       </div>
