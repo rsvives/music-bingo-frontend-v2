65c6cc0713e950dd93f2c1a7e4d66350ac52943b
Rodri <r.serranvives@gmail.com>
2025-09-29 19:41:14 +0200

    feat: debug footer

--- src/routes/_auth/route.tsx
+++ src/routes/_auth/route.tsx
@@ -1,5 +1,9 @@
 import { Outlet, createFileRoute, redirect } from '@tanstack/react-router'
 import { Aside } from '@/components/Aside'
+import { useGameStore } from '@/store/useGameStore'
+import { useRoomStore } from '@/store/useRoomStore'
+import { usePlayersStore } from '@/store/usePlayersStore'
+import { useNumbersStore } from '@/store/useNumbersStore'
 
 export const Route = createFileRoute('/_auth')({
     beforeLoad: ({ context, location }) => {
@@ -16,11 +20,32 @@ export const Route = createFileRoute('/_auth')({
 })
 
 const AuthRoute = () => {
+
+
     return (
         <div className='flex min-h-[80vh] flex-col sm:flex-row'>
             <Aside />
             <Outlet />
+            {/* <DebugFooter /> */}
         </div>
 
     )
+}
+
+const DebugFooter = () => {
+    const { gameStatus, } = useGameStore()
+    const { admin, code, roomId, isJoined } = useRoomStore()
+    const { myBingoNumbers, myMarkedNumbers, lastCalledNumber } = useNumbersStore()
+    const { currentPlayer, players } = usePlayersStore()
+    return (
+        <footer className='fixed bottom-0 bg-slate-800 text-white rounded-md p-2 w-[100%]'>
+            <p>
+                {JSON.stringify({ game: gameStatus })}
+            </p>
+            <p>{JSON.stringify({ room: { admin, code, roomId, isJoined } })}</p>
+            <p>{JSON.stringify({ numbers: { myBingoNumbers, myMarkedNumbers: [...myMarkedNumbers], lastCalledNumber } })}</p>
+            <p>{JSON.stringify({ roomPlayers: currentPlayer, players: [...players] })}</p>
+        </footer>
+    )
+
 }
