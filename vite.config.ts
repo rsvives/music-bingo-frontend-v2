7d39acd8bfa7494cac133cabf9cacc4beabe071f
Rodri <r.serranvives@gmail.com>
2025-11-05 20:34:59 +0100

    fix: lint issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,10 +12,10 @@ export default defineConfig({
     viteReact(),
     tailwindcss(),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  // },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
